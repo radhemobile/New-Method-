1e8b3060850a64dbea2958e8464a057763518b91
radhemobile <jakirhusain0254@gmail.com>
2024-12-23 19:07:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7709792223:AAEy3BwIheJ2ikrhrAsHKlHlTnKUQmZl7hs"
+ADMIN_IDS = ["1549748318"]
+GROUP_ID = -1002421000156  # Replace with your group ID
+GROUP_LINK = "https://t.me/+jbaG-YR7JGJlY2U1"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
